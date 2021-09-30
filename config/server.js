86e2fa2af4af5730c07e1e0927492dba7105d5c9
Ezequiel <marcos.ezequiel.lpz@gmail.com>
2021-09-30 15:50:15 -0300

    fixing database

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0fe6730b8b08db0013575e677f9cecaf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74f16f22-2476-4f2a-b15b-28518fc5d451'
+};
